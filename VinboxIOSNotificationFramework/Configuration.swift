1d01636523b91daca29adbbb7829f7f6e5b04846
ravi-vinmax <ravi@vinbox.in>
2022-11-19 14:51:21 +0000

    CBI ios branch base

--- VinboxIOSNotificationFramework/Configuration.swift
+++ VinboxIOSNotificationFramework/Configuration.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct Configuration{
-    static let GOOGLE_APP_ID = "1:351477624742:ios:0189e10dea3c9ef1ffe967";
-    static let GCM_SENDER_ID = "351477624742";
-    static let PROJECT_ID = "vinbox-ios-notification-a20a4";
-    static let API_KEY = "AIzaSyCcjrh2lbmKtiUWNUDrrO1jZD9kAJuabKc";
+    static let GOOGLE_APP_ID = "1:723309228396:ios:e909bc2505c5b6da5c4349";
+    static let GCM_SENDER_ID = "723309228396";
+    static let PROJECT_ID = "central-bank-of-india-3a489";
+    static let API_KEY = "AIzaSyCuoa_279cwh-jbYygnkBlSVrmwL2iIK8o";
     static let DATABASE_URL = "https://vinbox-ios-notification-a20a4-default-rtdb.firebaseio.com/";
     static let API_URL = "https://t1.vsu1t.com/fcmwebpush/apppush/subscription";
 }
