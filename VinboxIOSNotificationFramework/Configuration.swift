59d5cd5dd4652e7726a122e8264a6a214c5193a7
Ravi kumar <94934221+ravi-vinbox@users.noreply.github.com>
2025-01-21 17:39:30 -0500

    Update Configuration.swift

--- VinboxIOSNotificationFramework/Configuration.swift
+++ VinboxIOSNotificationFramework/Configuration.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct Configuration{
-    static let GOOGLE_APP_ID = "1:351477624742:ios:0189e10dea3c9ef1ffe967";
-    static let GCM_SENDER_ID = "351477624742";
-    static let PROJECT_ID = "vinbox-ios-notification-a20a4";
+    static let GOOGLE_APP_ID = "1:351477624744:ios:0189e10dea3c9ef1ffe968";
+    static let GCM_SENDER_ID = "351477624744";
+    static let PROJECT_ID = "vinbox-ios-notification-a20a6";
     static let API_KEY = "AIzaSyCcjrh2lbmKtiUWNUDrrO1jZD9kAJuabKc";
-    static let DATABASE_URL = "https://vinbox-ios-notification-a20a4-default-rtdb.firebaseio.com/";
+    static let DATABASE_URL = "https://vinbox-ios-notification-a20a6-default-rtdb.firebaseio.com/";
     static let API_URL = "https://digimate1.vinbox.in/fcmwebpush/apppush/subscription";
 }
